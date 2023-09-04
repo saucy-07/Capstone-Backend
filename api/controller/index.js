81346580a75d28121ae9cc9e2b516b36f08d440d
connor <connorb0706@gmail.com>
2023-09-04 11:22:06 +0200

    third-push

--- api/controller/index.js
+++ api/controller/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routes = express.Router()
 
-const {users} = require("../models")
+const {users, products} = require("../models")
 
 routes.get('/users', (req, res) =>{
     users.fetchUsers(req,res)
@@ -21,6 +21,25 @@ routes.patch('/user/:id', bodyParser.json(), (req, res) =>{
 })
 routes.delete('/user/:id', bodyParser.json(), (req, res) =>{
     users.deleteUser(req, res)
+})
+
+routes.get('/products', (req, res) =>{
+    products.fetchProducts(req, res)
+})
+routes.get('/product/:id', (req, res) =>{
+    products.fetchProduct(req, res)
+})
+routes.post('/product', bodyParser.json(), (req, res) =>{
+    products.addProduct(req, res)
+})
+routes.put('/product/:id', bodyParser.json(), (req, res) =>{
+    products.updateProduct(req, res)
+})
+routes.patch('/product/:id', bodyParser.json(), (req, res) =>{
+    products.updateProduct(req, res)
+})
+routes.delete('/product/:id', bodyParser.json(), (req, res) =>{
+    products.deleteProduct(req, res)
 })
 
 module.exports = {
