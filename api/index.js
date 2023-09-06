111bdcef329cca2837a3e1d3329d652a339597c6
connor <connorb0706@gmail.com>
2023-09-06 12:38:05 +0200

    eighth-push

--- api/index.js
+++ api/index.js
@@ -2,13 +2,15 @@ const {express, routes} = require('./controller')
 const path = require('path')
 const app = express()
 const port = +process.env.PORT || 3000
+const cors = require('cors')
 
 app.use(express.static('./static'))
 app.use(
     express.urlencoded({
         extended: false
     }),
-    routes
+    routes,
+    cors()
 )
 routes.get('./controller/index.js', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './static/html/index.html'))
